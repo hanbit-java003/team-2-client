ded27d03759eef2e5817ccf9fac423447df1a43a
ijy <sereinn@naver.com>
2017-10-24 17:14:11 +0900

    common color

--- src/js/common.js
+++ src/js/common.js
@@ -6,7 +6,25 @@ $('.header-menu-name').on('click', function () {
     location.href = '/' + id + '.html';
 });
 
+
 $('#index').on('click', function () {
     location.href = '/index.html';
 });
+
+
+/*$('#member-layout').on('click', function () {
+   var memberLayer = require('../template/member-login.hbs');
+
+    $('body').append(memberLayer);
+
+    $('.header-sub').animate({
+        right: '0px'
+    }, {
+        duration: 500,
+        complete: function () {
+            $('.header-sub').remove();
+            $('body').css('overflow', 'auto');
+        }
+    });
+});*/
 
