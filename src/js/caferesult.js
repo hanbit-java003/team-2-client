b1cdbdfa25342cb81cb00853c797ea99aeb84d5f
ijy <sereinn@naver.com>
2017-10-25 18:39:20 +0900

    login 창

--- src/js/caferesult.js
+++ src/js/caferesult.js
@@ -11,23 +11,20 @@ var cafeModel = {
         lat: 37.555087,
         lng: 126.926846
     },
-    way : {
-        traffic: {
-            bus: '153, 143, 162 승차 후 서강대학교 정류장에서 하차',
-            subway: '2호선 신촌역 6번출구에서 600M'
-        },
-        car : '홍대입구역에서 서교동교회 방면으로 5분'
+    traffic: {
+        bus: '153, 143, 162 승차 후 서강대학교 정류장에서 하차',
+        subway: '2호선 신촌역 6번출구에서 600M',
+        car: '홍대입구역에서 서교동교회 방면으로 5분'
     }
-
 };
 
 function initLocation() {
     var wayTraffic = $('#loc-traffic-cafe');
-    wayTraffic.append('버스 : ' + cafeModel.way.traffic.bus + '<br>' +
-        '지하철 : ' + cafeModel.way.traffic.subway);
+    wayTraffic.append('버스 : ' + cafeModel.traffic.bus + '<br>' +
+        '지하철 : ' + cafeModel.traffic.subway);
 
     var wayCar = $('#loc-car-cafe');
-    wayCar.append(cafeModel.way.car);
+    wayCar.append(cafeModel.traffic.car);
 
     var loadGoogleMapsApi = require('load-google-maps-api-2');
 
